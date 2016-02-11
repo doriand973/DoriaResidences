91676a0b10d98cbfb9f1e3f6c1230be71717b76d
doriand973 <andrea.g.doria@gmail.com>
2016-02-11 18:02:48 +0100

    Aggiornata Classe Tariffa Eliminazione Prezziario

--- DoriaResidences/Form4.vb
+++ DoriaResidences/Form4.vb
@@ -1,26 +1,31 @@
 ﻿Imports System.DateTime
 
 Public Class Form4
-    'Public Pio As String
+    Public Pio As String
     Private Sub Form4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim PrezziDoria As New Prezziario
-
+        Dim PrezziDoria As New List(Of Tariffa)
+        Dim Pio As String = ""
         '*************************************************************************
         '                         CREAZIONE PREZZIARIO 2016
         '*************************************************************************
-        PrezziDoria.Addtariffa(New Tariffa(New Date(2016, 05, 14), New Date(2016, 05, 28), 245, 285, 0, 300))
-        PrezziDoria.Addtariffa(New Tariffa(New Date(2016, 05, 28), New Date(2016, 06, 18), 300, 340, 0, 355))
-        PrezziDoria.Addtariffa(New Tariffa(New Date(2016, 06, 18), New Date(2016, 07, 02), 380, 445, 0, 460))
-        PrezziDoria.Addtariffa(New Tariffa(New Date(2016, 07, 02), New Date(2016, 07, 16), 450, 530, 0, 545))
-        PrezziDoria.Addtariffa(New Tariffa(New Date(2016, 07, 16), New Date(2016, 07, 30), 500, 590, 0, 605))
-        PrezziDoria.Addtariffa(New Tariffa(New Date(2016, 07, 30), New Date(2016, 08, 20), 595, 700, 0, 715))
-        PrezziDoria.Addtariffa(New Tariffa(New Date(2016, 08, 20), New Date(2016, 08, 27), 500, 590, 0, 605))
-        PrezziDoria.Addtariffa(New Tariffa(New Date(2016, 08, 27), New Date(2016, 09, 03), 450, 530, 0, 545))
-        PrezziDoria.Addtariffa(New Tariffa(New Date(2016, 09, 03), New Date(2016, 09, 10), 300, 340, 0, 355))
-        PrezziDoria.Addtariffa(New Tariffa(New Date(2016, 09, 10), New Date(2016, 09, 24), 245, 285, 0, 300))
-        PrezziDoria.Addtariffa(New Tariffa(New Date(2016, 09, 24), New Date(2016, 10, 01), 225, 255, 0, 270))
+        PrezziDoria.Add(New Tariffa(New Date(2016, 05, 14), New Date(2016, 05, 28), 245, 285, 0, 300))
+        PrezziDoria.Add(New Tariffa(New Date(2016, 05, 28), New Date(2016, 06, 18), 300, 340, 0, 355))
+        PrezziDoria.Add(New Tariffa(New Date(2016, 06, 18), New Date(2016, 07, 02), 380, 445, 0, 460))
+        PrezziDoria.Add(New Tariffa(New Date(2016, 07, 02), New Date(2016, 07, 16), 450, 530, 0, 545))
+        PrezziDoria.Add(New Tariffa(New Date(2016, 07, 16), New Date(2016, 07, 30), 500, 590, 0, 605))
+        PrezziDoria.Add(New Tariffa(New Date(2016, 07, 30), New Date(2016, 08, 20), 595, 700, 0, 715))
+        PrezziDoria.Add(New Tariffa(New Date(2016, 08, 20), New Date(2016, 08, 27), 500, 590, 0, 605))
+        PrezziDoria.Add(New Tariffa(New Date(2016, 08, 27), New Date(2016, 09, 03), 450, 530, 0, 545))
+        PrezziDoria.Add(New Tariffa(New Date(2016, 09, 03), New Date(2016, 09, 10), 300, 340, 0, 355))
+        PrezziDoria.Add(New Tariffa(New Date(2016, 09, 10), New Date(2016, 09, 24), 245, 285, 0, 300))
+        PrezziDoria.Add(New Tariffa(New Date(2016, 09, 24), New Date(2016, 10, 01), 225, 255, 0, 270))
 
-        ' Pio = PrezziDoria.ToString
+
+        For i As Integer = 0 To PrezziDoria.Count - 1 Step 1
+
+            Pio = Pio & PrezziDoria.Item(i).ToString & vbNewLine
+
+        Next
 
     End Sub
 End Class
--- DoriaResidences/Tariffa.vb
+++ DoriaResidences/Tariffa.vb
@@ -1,47 +1,48 @@
 ﻿Imports System.DateTime
 Public Class Tariffa
-    Private dfrom As DateTime
-    Private dto As DateTime
+    Private dfrom As Date
+    Private dto As Date
     Private bD1 As Integer
     Private tD1 As Integer
     Private bD2 As Integer
     Private tD2 As Integer
 
-    Public Sub New(ByVal dfrom As Date, ByVal dto As Date, ByVal bD1 As Integer, ByVal tD1 As Integer, ByVal bD2 As Integer, ByValtD2 As Integer)
-        dfrom = Me.dfrom
-        dto = Me.dto
-        bD1 = Me.bD1
-        bD2 = Me.bD2
-        tD1 = Me.tD1
-        tD2 = Me.tD2
+    Public Sub New(ByVal dfrom As Date, ByVal dto As Date, ByVal bD1 As Integer, ByVal tD1 As Integer, ByVal bD2 As Integer, ByVal tD2 As Integer)
+        Me.dfrom = dfrom
+        Me.dto = dto
+        Me.bD1 = bD1
+        Me.bD2 = bD2
+        Me.tD1 = tD1
+        Me.tD2 = tD2
     End Sub
-   
+
     Function getD_start() As Date
-        Return Me.dfrom
+        Return dfrom
     End Function
     Sub setD_start(ByVal d As Date)
-        d = Me.dfrom
+        dfrom = d
     End Sub
     Function getD_end() As Date
-        Return Me.dto
+        Return dto
     End Function
     Sub setD_end(ByVal d As Date)
-        d = Me.dto
+        dto = d
     End Sub
     Function getbiloD1() As Integer
-        Return Me.bD1
+        Return bD1
     End Function
     Function getbiloD2() As Integer
-        Return Me.bD2
+        Return bD2
     End Function
     Function gettriloD1() As Integer
-        Return Me.tD1
+        Return tD1
     End Function
     Function gettriloD2() As Integer
-        Return Me.tD2
+        Return tD2
     End Function
-    Function getWeek(d As Date) As Integer
-        Return DatePart(DateInterval.WeekOfYear, d)
+    ' Funzione che mi restituisce il numero della settimana dell'anno dato il giorno
+    Private Function getWeek(d As Date) As Integer
+        Return DatePart(DateInterval.WeekOfYear, d) - 1
     End Function
     Function getWeek_start() As Integer
         Return getWeek(dfrom)
@@ -67,5 +68,10 @@ Public Class Tariffa
             Return -1
         End Try
 
+    End Function
+    Overrides Function ToString() As String
+        Dim s As String
+        s = Me.dfrom.ToString("d/MM/yyyy") & " , " & Me.dto.ToString("d/MM/yyyy") & " , " & Me.bD1.ToString & " , " & Me.tD1.ToString & " , " & Me.bD2.ToString & " , " & Me.tD2.ToString
+        Return s
     End Function
 End Class
